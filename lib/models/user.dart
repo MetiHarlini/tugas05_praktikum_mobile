7db686f38cc24a2a0db49beec6150ef663d4d8fc
MetiHarlini <metiharlini31@gmail.com>
2023-03-29 17:01:19 +0700

    first commit

---
+++ lib/models/user.dart
@@ -0,0 +1,40 @@
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final String? profilePhoto;
+  final String? phoneNumber;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    this.profilePhoto,
+    this.phoneNumber,
+  });
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map["id"],
+      name: map["name"],
+      username: map["username"],
+      email: map["email"],
+      profilePhoto: map["profile_photo"],
+      phoneNumber: map["phone_number"],
+    );
+  }
+
+  factory User.dummy() {
+    return User(
+      id: 001,
+      name: "Meti Harlini",
+      username: "Meti",
+      email: "2006102@itg.ac.id",
+      profilePhoto:
+          "https://i.postimg.cc/9fD66SBn/DSC-3941.jpg",
+      phoneNumber: "089608034765",
+    );
+  }
+}
---
+++ lib/modules/news_detail_screen/news_detail_screen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class NewsDetailScreen extends StatefulWidget {
+  const NewsDetailScreen({
+    super.key,
+    required this.id,
+  });
+  final String id;
+
+  @override
+  State<NewsDetailScreen> createState() => _NewsDetailScreenState();
+}
+
+class _NewsDetailScreenState extends State<NewsDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              "https://i.postimg.cc/SxgBnqss/Gambar-ucapan-selamat-berbuka-puasa-Ramadhan-2023-4.jpg",
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  children: [
+                   const SizedBox(height: 10),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment:MainAxisAlignment.center ,
+                      children: const [
+                        Text(
+                          'Berbuka Puasa Di Bulan Ramadhan',
+                           style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                  const Text(
+                    'Selamat Berbuka Puasa',
+                    
+                    maxLines: 10,
+                    textAlign: TextAlign.justify,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                  ],
+                ),
+              ),
+          ],
+          
+        ),
+      ),
+    );
+  }
+}
+
+class NewsDetailScreenHot extends StatefulWidget {
+  const NewsDetailScreenHot({
+    super.key, 
+    required this.newsDetailHot,
+  });
+  final String newsDetailHot;
+
+  @override
+  State<NewsDetailScreenHot> createState() => _NewsDetailScreenHotState();
+}
+
+class _NewsDetailScreenHotState extends State<NewsDetailScreenHot> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              "https://i.postimg.cc/xC2h7dp6/inspirasi-menu-ramadhan-sahur-buka-puasa-30-hari-2023.jpg",
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  children: [
+                   const SizedBox(height: 10),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment:MainAxisAlignment.center ,
+                      children: const [
+                        Text(
+                          '30 Hari Inspirasi Menu Ramadhan',
+                           style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                  const Text(
+                    'Resep Sahur Dan Buka Puasa Di Bulan Ramadhan',
+                    maxLines: 10,
+                    textAlign: TextAlign.justify,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                  ],
+                ),
+              ),
+          ],
+          
+        ),
+      ),
+    );
+  }
+}
